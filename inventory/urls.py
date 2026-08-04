52a9331a7bdbddec75dcb35981986d018bee6ab7
Levent <leventbilal.16@outlook.com>
2026-08-04 16:33:47 +0200

    Initial template for menu page prepared.

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('menus', views.MenuView.as_view(), name='menus'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "inventory/home.html"
+
+class MenuView(TemplateView):
+    template_name = "inventory/menus.html"
